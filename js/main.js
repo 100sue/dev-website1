d9a19c290358ab8c5d9274a97285ad7b5f7c88b9
appert <scoreur@gmail.com>
2022-12-03 10:09:47 +0100

    ajout scrollbar

--- js/main.js
+++ js/main.js
@@ -32,6 +32,29 @@ const menu = document.querySelector('.menu');
 const menuBtn = document.querySelector('.menu-btn');
 
 menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('menu-open')
+    menu.classList.toggle('menu-open');
 
 });
+
+// Scroll Events :
+
+const pagination = document.querySelectorAll('.pagination li a');
+const sections = document.querySelectorAll('.main-section');
+const offset = 100 ;
+
+
+// Loop through sections
+
+// for(let i = 0; i < sections.lenght; i++) { 
+
+//window.addEventListener('scroll', () =>
+
+    //if(window.scrollY > sections[i].offsetTop - offset && window.scrollY > sections[i].offsetTop + sections[i].offsetHeight - offset) {
+            //pagination.forEach(pag => {
+              //  pag.classList.remove('active');
+            //});
+            pagination[i].classList.add('active');
+    //      } 
+    //  }) ;
+  //}
+
